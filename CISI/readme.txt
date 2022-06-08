940731df85ad26c7f104ba1c1671f6313b0e9934
zainabali <za562925@gmail.com>
2022-06-08 06:33:56 +0300

    first commit

---
+++ CISI/readme.txt
@@ -0,0 +1,4 @@
+CICI.ALL A file of 1,460 "documents" each with a unique ID (.I), title (.T), author (.A), abstract (.W) and list of cross-references to other documents (.X). 
+CISI.QRY Original text of the query
+CISI.REL relation giving query_id document_id 0 0 to indicate dument did is relevant to query qid
+75 qry used
---
+++ cosin_sim.py
@@ -0,0 +1,27 @@
+import math
+import re
+from collections import Counter
+import textPro
+
+WORD = re.compile(r"\w+")
+
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = textPro.qrtest_pr(text)
+    #print(words)
+    return Counter(words)
+
